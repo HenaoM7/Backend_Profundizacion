a0d1a2c625b85eb7aef47549f3130e70bea62d58
Ximenaphz <ximena.pinoh@comunidad.iush.edu.co>
2026-05-16 10:41:00 -0500

    Update Column -creado_por- Database

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -1,5 +1,5 @@
 import crypto from 'node:crypto';
 
 export const generateEntityId = (prefix) => {
-  return `${prefix}${crypto.randomUUID().replace(/-/g, '').slice(0, 12).toUpperCase()}`;
+  return crypto.randomUUID();
 };
